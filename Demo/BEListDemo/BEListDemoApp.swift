7215deb419d35c5f4dfd655ab7d8543c13de2e0b
Chung Tran <bigearsenal@gmail.com>
2022-08-18 16:22:20 +0700

    add Demo project

---
+++ Demo/BEListDemo/BEListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BEListDemoApp.swift
+//  BEListDemo
+//
+//  Created by Chung Tran on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BEListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
