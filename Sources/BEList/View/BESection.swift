896db8d1188d6dbddf59bd5f1438ff37caf6b4f3
Chung Tran <bigearsenal@gmail.com>
2022-08-18 15:30:52 +0700

    feat: overload initializer for BESection

--- Sources/BEList/View/BESection.swift
+++ Sources/BEList/View/BESection.swift
@@ -16,7 +16,7 @@ extension BESection: BESectionType {
 public struct BESection<
     HeaderView: View,
     CellView: View,
-    EmptyView: View,
+    EmptView: View,
     LoadingView: View,
     FooterView: View
 >: View {
@@ -27,27 +27,42 @@ public struct BESection<
     public let data: BESectionData
     
     /// Optional: Builder for the view on top of everything in ListView
-    @ViewBuilder public var headerView: HeaderView
+    @ViewBuilder public var headerView: () -> HeaderView
     
     /// Optional: Builder for ListView when data is empty
-    @ViewBuilder public var emptyView: EmptyView
+    @ViewBuilder public var emptyView: () -> EmptView
     
     /// Optional: Builder for ListView when data is loading
-    @ViewBuilder public var loadingView: LoadingView // Optional
+    @ViewBuilder public var loadingView: () -> LoadingView // Optional
     
     /// Optional: Builder for the view at the bottom of everything in ListView
-    @ViewBuilder public var footerView: FooterView
+    @ViewBuilder public var footerView: () -> FooterView
     
     /// Builder for each cell of the section
     public let cellBuilder: (AnyHashable) -> CellView
     
-    // MARK: - Private properties
+    // MARK: - Initializer
+    public init(
+        data: BESectionData,
+        headerView: @escaping () -> HeaderView,
+        emptyView: @escaping () -> EmptView,
+        loadingView: @escaping () -> LoadingView,
+        footerView: @escaping () -> FooterView,
+        cellBuilder: @escaping (AnyHashable) -> CellView
+    ) {
+        self.data = data
+        self.headerView = headerView
+        self.emptyView = emptyView
+        self.loadingView = loadingView
+        self.footerView = footerView
+        self.cellBuilder = cellBuilder
+    }
     
     /// Body of the section
     public var body: some View {
         Group {
             if data.state == .loaded && data.items.isEmpty {
-                emptyView
+                emptyView()
             } else {
                 // all items
                 ForEach(data.items, id: \.hashValue) { item in
@@ -56,9 +71,70 @@ public struct BESection<
                 
                 // loading
                 if data.state == .loading || data.state == .initializing {
-                    loadingView
+                    loadingView()
                 }
             }
         }
+    }
+}
+
+// MARK: - Overload initializers
+
+extension BESection where HeaderView == EmptyView {
+    /// Overload initializer to support optional HeaderView
+    public init(
+        data: BESectionData,
+        emptyView: @escaping () -> EmptView,
+        loadingView: @escaping () -> LoadingView,
+        footerView: @escaping () -> FooterView,
+        cellBuilder: @escaping (AnyHashable) -> CellView
+    ) {
+        self.init(
+            data: data,
+            headerView: { EmptyView() },
+            emptyView: emptyView,
+            loadingView: loadingView,
+            footerView: footerView,
+            cellBuilder: cellBuilder
+        )
+    }
+}
+
+extension BESection where FooterView == EmptyView {
+    /// Overload initializer to support optional FooterView
+    public init(
+        data: BESectionData,
+        headerView: @escaping () -> HeaderView,
+        emptyView: @escaping () -> EmptView,
+        loadingView: @escaping () -> LoadingView,
+        cellBuilder: @escaping (AnyHashable) -> CellView
+    ) {
+        self.init(
+            data: data,
+            headerView: headerView,
+            emptyView: emptyView,
+            loadingView: loadingView,
+            footerView: { EmptyView() },
+            cellBuilder: cellBuilder
+        )
+    }
+}
+
+extension BESection where HeaderView == EmptyView, FooterView == EmptyView {
+    /// Overload initializer to support optional HeaderView & FooterView
+    public init(
+        data: BESectionData,
+        emptyView: @escaping () -> EmptView,
+        loadingView: @escaping () -> LoadingView,
+        cellBuilder: @escaping (AnyHashable) -> CellView
+    ) {
+        self.init(
+            data: data,
+            headerView: { EmptyView() },
+            emptyView: emptyView,
+            loadingView: loadingView,
+            footerView: { EmptyView() },
+            cellBuilder: cellBuilder
+        )
     }
 }
